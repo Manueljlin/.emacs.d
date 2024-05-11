e6a8c9db10ee294eb1e0f851677b967e31f64150
Manuel Jesús de la Fuente <m@nueljl.in>
2024-05-11 02:29:57 +0200

    move to docker with rocm

--- config/llm.el
+++ config/llm.el
@@ -33,28 +33,49 @@
                                  :chat-model "llama3-70b-8192"
                                  :embedding-model "llama3-70b-8192"
                                  :url "https://api.groq.com/openai/v1/")))
-  (defun mn/pull-llms ()
+
+  (defun mn/create-ollama-docker ()
+    (interactive)
+    (async-shell-command
+     (concat "docker volume create ollama-data && "
+             "docker volume create ollama-config"))
+    (async-shell-command
+     (concat "docker run -d "
+             "--device /dev/kfd --device /dev/dri "
+             "-v ollama-data:/root/.ollama/models "
+             "-v ollama-config:/root/.ollama/config "
+             "-p 11434:11434 "
+             "--name ollama "
+             "-e HSA_OVERRIDE_GFX_VERSION=10.3.0 "
+             "ollama/ollama:rocm")))
+
+  (defun mn/start-ollama-docker ()
+    (interactive)
+    (async-shell-command "docker start -a ollama"))
+
+  (defun mn/stop-ollama-docker ()
+    (interactive)
+    (async-shell-command "docker stop ollama"))
+
+  (defun mn/nuke-ollama-docker ()
+    (interactive)
+    (async-shell-command "docker rm ollama"))
+
+  (defun mn/pull-ollama-llms ()
     "Pull LLMs with Ollama CLI"
     (interactive)
     (mapc
      (lambda (llm)
        (let ((model-name (llm-ollama-chat-model (symbol-value llm))))
          (message (concat "Pulling " model-name))
-         (async-shell-command (concat "ollama pull " model-name))))
+         (async-shell-command (concat "docker exec -it ollama ollama pull " model-name))))
      '(mn/llm::phi3 mn/llm::wizardlm2)))
 
   :config
   (setopt ellama-language "English")
 
-  (setopt ellama-provider mn/llm::groq-llama3-70b)
-  ;; (setopt ellama-provider mn/llm::phi3)
-  (setopt ellama-naming-provider mn/llm::phi3)
-
-  ;; (setopt ellama-provider
-  ;;         (make-llm-ollama :chat-model "phi3:instruct"
-  ;;                          :embedding-model "phi3:instruct"))
-  ;; (setopt ellama-naming-provider
-  ;;         (make-llm-ollama :chat-model "phi3:instruct"
-  ;;                          :embedding-model "phi3:instruct"))
-
-  (setopt ellama-naming-scheme 'ellama-generate-name-by-llm))
+  (setopt ellama-provider mn/llm::wizardlm2) ; default LLM
+  (setopt ellama-naming-provider mn/llm::wizardlm2)
+  (setopt ellama-providers '(("Phi-3 3.8B Instruct"   . mn/llm::phi3)
+                             ("WizardLM2 7B Instruct" . mn/llm::wizardlm2)
+                             ("Groq Llama 3 70B"      . mn/llm::groq-llama3-70b))))
